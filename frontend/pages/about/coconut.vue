195fb8206c405e29977c5c1b1ec8eff09a75051b
XguoX <xguox@hotmail.com>
2018-08-08 22:58:03 +0800

    frontend: vue nuxt element-ui

---
+++ frontend/pages/about/coconut.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>CoCoNut</h2>
+</template>
---
+++ frontend/plugins/element-ui.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+// 全部引用，此时需要在nuxt.config.js中设置css
+Vue.use(require('element-ui'))
+
+// 按需引用
+// import { Button } from 'element-ui'
+// Vue.component(Button.name, Button)
