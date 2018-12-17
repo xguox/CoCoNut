62ed755b2a6338ae1b7ce702072ef79443d8123f
XguoX <xguox@hotmail.com>
2018-12-17 16:17:43 +0800

    docker for standalone_migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180918135859) do
+ActiveRecord::Schema.define(version: 2018_09_18_135859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
