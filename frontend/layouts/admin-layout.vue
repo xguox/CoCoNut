ca949758cd109dc70e444e81669f30294d39c41e
sgemini <s13268001833@gmail.com>
2019-03-16 23:20:50 +0800

    remove vue

--- frontend/layouts/admin-layout.vue
+++
@@ -1,6 +0,0 @@
-<template>
- <div>
-   <h1>Admin Layout</h1>
-   <nuxt />
- </div>
-</template>
--- frontend/pages/products/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Products Page</h1>
-    <p>This is another basic page demonstrating a generate route</p>
-  </div>
-</template>
