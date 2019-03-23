c481928d3bebc4086b466b23cf3ebdfb013b5f16
sgemini <s13268001833@gmail.com>
2019-03-23 15:40:07 +0800

    build react frontend

---
+++ frontend/src/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends Component{
+    render(){
+        return (
+            <div id="page-wrapper">hels222lo COCO</div>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
