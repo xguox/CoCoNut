4e45abc8e1191aabaf60b17d7c0cf302cdd19a2c
vliang <vliang@gz-amy.intranet.theknot.com>
2019-04-08 19:09:05 +0800

    wip

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-
-// import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './page/home/index.jsx';
+import Login from './page/login/index.jsx';
+
+import './app.scss';
 
 class App extends React.Component {
   render() {
     return(
       <Router>
         <Switch>
-          <Route exact component={Home} path='/' />
+          <Route exact component={Home} path='/admin' />
+          <Route exact component={Login} path='/admin/login' />
           {/* <Route exact component={AnimalIndex} path='/animals' />
           <Route exact component={AdoptionTips} path='/adoption-tips' />
           <Route exact component={About} path='/about' />
@@ -20,11 +21,8 @@ class App extends React.Component {
           <Route exact component={AnimalShow} path='/animals/:id' /> */}
         </Switch>
       </Router>
-
-      // <div>123</div>
     )
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
---
+++ frontend/src/page/login/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './login.scss';
+
+export default class Login extends Component {
+    render() {
+        return(
+            <div className='admin-login-container'>
+              <h1 className='admin-login-title'>COCONUT</h1>
+              <form>
+                <input placeholder='name' />
+                <input placeholder='pwd' />
+                <button>登录</button>
+              </form>
+            </div>
+        )
+    }
+}
