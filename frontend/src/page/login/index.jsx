969afcca03f47ec5c70e09e6906c1535c953f8f3
vliang <vliang@gz-amy.intranet.theknot.com>
2019-04-12 20:32:08 +0800

    wip

--- frontend/src/page/login/index.jsx
+++ frontend/src/page/login/index.jsx
@@ -5,9 +5,9 @@ import './login.scss';
 export default class Login extends Component {
     render() {
         return(
-            <div className='admin-login-container'>
-              <h1 className='admin-login-title'>COCONUT</h1>
-              <form>
+            <div className='admin-login--container'>
+              <h1 className='admin-login--title'>COCONUT</h1>
+              <form className='admin-login--form'>
                 <input placeholder='name' />
                 <input placeholder='pwd' />
                 <button>登录</button>
